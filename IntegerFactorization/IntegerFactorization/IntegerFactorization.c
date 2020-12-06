4ef752f5f0b783d9a7a72f07eda34a818d5e0558
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-06 13:56:05 +0100

    done

---
+++ IntegerFactorization/IntegerFactorization/IntegerFactorization.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void factorize(int num)
+{
+	int divisor = 2;
+	while (num > 1)
+	{
+		if (num % divisor != 0)
+		{
+			divisor++;
+		}
+		else 
+		{
+			printf_s("%5d", num);
+			printf_s("|");
+			printf_s("%d", divisor);
+			printf_s("\n");
+
+			num = num / divisor;
+		}
+	}
+	printf_s("%5d", 1);
+	printf_s("|");
+}
+
+int main()
+{
+	int inputNum;
+	printf_s("Integer Factorizazion Calculator\n\n");
+	printf_s("Please enter number:\n\n");
+	scanf_s("%d", &inputNum);
+
+	factorize(inputNum);
+
+	return 0;
+}
